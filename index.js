4ad2518492aa02b55bb12105bda9f65992a3286b
Luke Aeschleman <lukeaeschleman@gmail.com>
2021-03-05 17:21:14 -0500

    rename client

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
 
 let DdsClient = require('./lib/ddsClient');
+let DdsSqlClient = require('./lib/ddsSqlClient');
 
 exports.DdsClient = DdsClient;
 exports.createDdsClient = function (config) {
     return new DdsClient(config)
+}
+
+exports.DdsSqlClient = DdsSqlClient;
+exports.createDdsSqlClient = function (config) {
+    return new DdsSqlClient(config)
 }
 
 
