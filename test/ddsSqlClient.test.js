9fcb50cc4cae28f5c2ddd31aa624b850676cf9dd
Luke Aeschleman <lukeaeschleman@gmail.com>
2021-03-10 08:56:56 -0500

    remove old client, all tests passing

--- test/ddsSqlClient.test.js
+++ test/ddsSqlClient.test.js
@@ -16,5 +16,5 @@ test('throws error with bad config', () => {
         .select('data')
         .whereIn('$data.notes:array', ['i', 'h'])
         .whereIn('$data.grad_requirements:array', ['jec'])
-    expect(client.qb.sql()).toBe('column')
+    expect(client.qb.sql()).toBe("SELECT data FROM courses CROSS APPLY OPENJSON (data, '$.notes') WITH (notes NVARCHAR(max) '$') CROSS APPLY OPENJSON (data, '$.grad_requirements') WITH (grad_requirements NVARCHAR(max) '$') WHERE notes IN ('i','h') AND grad_requirements IN ('jec') ORDER BY courses._id OFFSET 0 ROWS FETCH NEXT 100 ROWS ONLY")
 });
